eb35ae70a6b84833fbc7c00c1f9604665c9a4b70
exact-dev <eet5ght4493@gmail.com>
2023-02-13 18:24:09 +0900

    firstjun

---
+++ server/src/main/java/com/preproject_009/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.preproject_009.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
