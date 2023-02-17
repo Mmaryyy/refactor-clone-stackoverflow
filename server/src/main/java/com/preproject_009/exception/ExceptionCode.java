5ea4768d2a6cdd34ae8eb93bfe27d245ff33cf9e
minjuxpp <minjuxpp@naver.com>
2023-02-17 17:19:57 +0900

    Question Service 1st commit : not finished / ExceptionCode Conflict 해결

--- server/src/main/java/com/preproject_009/exception/ExceptionCode.java
+++ server/src/main/java/com/preproject_009/exception/ExceptionCode.java
@@ -6,9 +6,7 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
     NOT_IMPLEMENTATION(501, "Not Implementation"),
-    INVALID_MEMBER_STATUS(400, "Invalid member status"),
-    QUESTION_NOT_FOUND(404, "Question not found"),
-    CANNOT_CHANGE_QUESTION(403, "Q&A Question can not change");
+    INVALID_MEMBER_STATUS(400, "Invalid member status");
     @Getter
     private int status;
 
