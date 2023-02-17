190b02b7be137f8289cc3bb53efc85c8437ca14a
Mmaryyy <marynrvly@gmail.com>
2023-02-17 14:15:59 +0900

    [docs] dummydata 추가

--- client/src/page/Contents.js
+++ client/src/page/Contents.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { SubmitButton } from '../styles/styledcomponents'
 import Content from '../components/Content'
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
