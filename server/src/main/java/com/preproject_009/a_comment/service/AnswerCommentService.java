6604979b3f109b791b7a39d60198e55a5d45941d
Hoodie1368 <jhj5375@gmail.com>
2023-02-17 11:04:01 +0900

    commit

--- server/src/main/java/com/preproject_009/a_comment/service/AnswerCommentService.java
+++ server/src/main/java/com/preproject_009/a_comment/service/AnswerCommentService.java
@@ -1,4 +1,34 @@
 package com.preproject_009.a_comment.service;
 
+import com.preproject_009.a_comment.entity.AnswerComment;
+import com.preproject_009.a_comment.repository.AnswerCommentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AnswerCommentService {
+    private final AnswerCommentRepository answerCommentRepository;
+
+    public AnswerCommentService(AnswerCommentRepository answerCommentRepository) {
+        this.answerCommentRepository = answerCommentRepository;
+    }
+
+    public AnswerComment createAnswerComment(AnswerComment answerComment) {
+        return null;
+    }
+
+    public AnswerComment updateAnswerComment(AnswerComment answerComment) {
+        return null;
+    }
+
+    public AnswerComment getAnswerComment(long answerCommentId) {
+        return null;
+    }
+
+    public AnswerComment getAnswerComments() {
+        return null;
+    }
+
+    public AnswerComment deleteAnswerComment(long answerCommentId) {
+        return null;
+    }
 }
--- server/src/main/java/com/preproject_009/answer/service/AnswerService.java
+++ server/src/main/java/com/preproject_009/answer/service/AnswerService.java
@@ -1,4 +1,36 @@
 package com.preproject_009.answer.service;
 
+import com.preproject_009.answer.entity.Answer;
+import com.preproject_009.answer.repository.AnswerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public AnswerService(AnswerRepository answerRepository) {
+        this.answerRepository = answerRepository;
+    }
+
+    public Answer createAnswer(Answer answer) {
+        return null;
+    }
+
+    public Answer updateAnswer(Answer answer) {
+        return null;
+    }
+
+    public Answer getAnswer(long answerId) {
+        return null;
+    }
+
+    public Answer getAnswers() {
+        return null;
+    }
+
+    public Answer deleteAnswer(long answerId) {
+        return null;
+    }
+
+
 }
