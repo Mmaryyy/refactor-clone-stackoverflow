53bb0e484c777e3075003382f5cb05524b98bd69
skuxyx <skubaltz@naver.com>
2023-02-20 17:20:33 +0900

    [feat] 마이페이지, 아바타, 프로필 추가

--- client/src/components/Mypage/Avatar.js
+++ client/src/components/Mypage/Avatar.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+
 export const UserProfiles = styled.div`
+  margin: 30px;
   width: ${(props) => props.width};
   height: ${(props) => props.height};
   border-radius: 5%;
+
+  background-image: ${(props) =>
+    props.img
+      ? `url(${props.img})`
+      : `url("https://placeimg.com/200/100/any")`};
 `;
 
 export default function Avatar({ width, height }) {
