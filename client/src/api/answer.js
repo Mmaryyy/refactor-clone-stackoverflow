3b113a850b4c4dd1164940e018d4e69382aee9ca
Mmaryyy <marynrvly@gmail.com>
2023-02-28 18:31:37 +0900

    [feat, style] api 수정 - 통합, redux 세팅,

--- client/src/api/answer.js
+++ client/src/api/answer.js
@@ -1,34 +1,35 @@
 import axios from "axios"
-import { getSingleContent } from "./question"
-import contents from '../datas/contents.json'
-import answer from '../datas/answers.json'
 
-export const addAnswer = async (questionId, currentUserId, content) => {
+export const addAnswer = (questionId, currentUserId, content) => {
     const id = Number(questionId)
     const requestBody = {
         memberId: currentUserId,
         questionId: id,
         content
     }
-    const result = await axios.post(`/api/questions/${id}/answers`, requestBody)
-                              .then(res => res)
-                              .catch(error => console.log(error))
-    return result
+    // answer create API 요청 보내기
+    axios.post(`/api/questions/${id}/answers`, requestBody)
+         .then(res => res)
+         .catch(error => console.log(error))
 }
-export const patchAnswer = async (questionId, answerId) => {
+export const patchAnswer = (answerId, content) => {
     const id = Number(answerId)
-    const result = await axios.patch(`/api/answers/${id}`)
-                              .then(res => res)
-                              .catch(error => console.log(error))
-    // answer update api 요청
-    const { answer } = getSingleContent(questionId)
-    // 업데이트된 질문 내용 받아서 answer 구조분해 해서 리턴
-    return answer
+    const requestBody = {
+        answerId: id,
+        content
+    }
+    axios.patch(`/api/answers/${answerId}`, requestBody)
+         .then(res => res)
+         .catch(error => console.log(error))
 }
-export const deleteAnswer = async (answerId) => {
-    const id = Number(answerId)
-    await axios.delete(`/api/answers/${id}`)
-               .then(res => res)
-               .catch(error => console.log(error))
+export const deleteAnswer = (answerId) => {
+    axios.delete(`/api/answers/${answerId}`)
+         .then(res => res)
+         .catch(error => console.log(error))
     
+}
+export const adoptAnswer = (memberId, questionId, answerId) => {
+    axios.patch(`/api/members/${memberId}/questions/${questionId}/answers/${answerId}`)
+         .then(res => res.data)
+         .catch(error => console.log(error))
 }
--- client/src/redux/reducers/contentsReducer.js
+++ client/src/redux/reducers/contentsReducer.js
@@ -1,13 +1,10 @@
-import { GET_SINGLE_CONTENT, GET_CONTENT_LIST, VOTE_UP, SET_CURRENT_CONTENT, CLEAR_CURRENT_CONTENT } from "../actions/contents"
+import { GET_SINGLE_CONTENT, GET_CONTENT_LIST, VOTE_UP, SET_CURRENT_CONTENT, ADD_QUESTION_COMMENT, UPDATE_CONTENT, GET_TAG_LIST } from "../actions/contents"
 const initialState = {
     contentList: [],
-    currentContent: {
-        content: {},
-        author: {},
-        answer: []
-    },
-    currentVote: 0,
-    currentPostContent: {}
+    pageInfo: {},
+    currentContent: {},
+    currentPostContent: {},
+    tagList: []
 }
 
 export const contentsReducer = (state = initialState, action) => {
@@ -20,7 +17,13 @@ export const contentsReducer = (state = initialState, action) => {
     case GET_CONTENT_LIST:
       return {
         ...state,
-        contentList: action.payload.contents,
+        contentList: action.payload.contentList,
+        pageInfo: action.payload.pageInfo
+      };
+    case UPDATE_CONTENT:
+      return {
+        ...state,
+        currentContent: action.payload
       };
     case VOTE_UP:
       return {
@@ -31,11 +34,16 @@ export const contentsReducer = (state = initialState, action) => {
       return {
         ...state,
         currentPostContent: action.payload
-      }
-    case CLEAR_CURRENT_CONTENT:
+      };
+    case ADD_QUESTION_COMMENT:
       return {
         ...state,
-        currentPostContent: action.payload
+        currentContent: action.payload
+      };
+    case GET_TAG_LIST:
+      return {
+        ...state,
+        tagList: action.payload
       }
     default:
       return state;
